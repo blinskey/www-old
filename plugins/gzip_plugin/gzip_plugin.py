6f12b8260808f29cdeade27584c2913858a45303
Benjamin Linskey <git@linskey.org>
2017-06-28 19:15:44 -0400

    Remove Python 2 code from gzip_plugin
    Removed old __future__ imports from original Python 2 version of code.

--- plugins/gzip_plugin/gzip_plugin.py
+++ plugins/gzip_plugin/gzip_plugin.py
@@ -1,10 +1,5 @@
 """Pelican plugin that gzips all files."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import gzip
 import os
 import re
