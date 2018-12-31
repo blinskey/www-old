815c2b7b966fe7c263c0038d4ac3ef9040d8fc80
Benjamin Linskey <git@linskey.org>
2018-12-30 19:05:13 -0500

    Refactor gzip plugin

--- plugins/gzip_plugin/gzip_plugin.py
+++ plugins/gzip_plugin/gzip_plugin.py
@@ -8,23 +8,22 @@ import shutil
 import pelican
 
 
-def for_each_file(pelican, func, pattern=".*"):
-    out_dir = pelican.settings["OUTPUT_PATH"]
+def compress(path):
+    with open(path, "rb") as in_file:
+        with gzip.open(path + ".gz", "wb") as out_file:
+            shutil.copyfileobj(in_file, out_file)
+
+
+def gzip_files(pelican):
+    pattern = ".*[^.gz]$"
     regex = re.compile(pattern)
+
+    out_dir = pelican.settings["OUTPUT_PATH"]
     for dirpath, dirnames, filenames in os.walk(out_dir):
         for name in filenames:
             if regex.match(name):
                 fullpath = os.path.join(dirpath, name)
-                func(fullpath)
-
-
-def gzip_files(pelican):
-    def compress(path):
-        with open(path, "rb") as in_file, \
-                gzip.open(path + ".gz", "wb") as out_file:
-            shutil.copyfileobj(in_file, out_file)
-
-    for_each_file(pelican, compress, pattern=".*[^\.gz]$")
+                compress(fullpath)
 
 
 def register():
